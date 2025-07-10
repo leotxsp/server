b6f111b04a38087697a873a8fff233d1840ae05a
leotxsp <leotxsp40@gmail.com>
2025-07-09 21:32:53 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import {fastify} from 'fastify';
+import {
+    serializerCompiler,
+    validatorCompiler,
+    type ZodTypeProvider,
+
+} from 'fastify-type-provider-zod';
+
+import {fastifyCors} from '@fastify/cors';
+import { env } from './env.ts';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>( )
+
+app.register(fastifyCors, {
+    origin: 'localhost:5173',
+})
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get('/health',() => {
+    return 'ok'
+})
+
+
+
+
+
+
+app.listen({port: env.PORT}).then(() => {
+    console.log(`Port: ${process.env.PORT}`)
+    console.log('Server is running on http://localhost:3333')
+})
