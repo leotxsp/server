da6f9dd339e0ae8b7f829fd37cdb940fc98840e1
leotxsp <leotxsp40@gmail.com>
2025-07-10 20:43:01 -0300

    Feat: connection with postgres

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,27 @@
-import {fastify} from 'fastify';
+import { fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify';
 import {
-    serializerCompiler,
-    validatorCompiler,
-    type ZodTypeProvider,
-
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
-
-import {fastifyCors} from '@fastify/cors';
+import { sql } from './db/connection.ts';
 import { env } from './env.ts';
 
-const app = fastify().withTypeProvider<ZodTypeProvider>( )
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-    origin: 'localhost:5173',
-})
+  origin: 'localhost:5173',
+});
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
-app.get('/health',() => {
-    return 'ok'
-})
+app.get('/health', () => {
+  return 'ok';
+});
 
-
-
-
-
-
-app.listen({port: env.PORT}).then(() => {
-    console.log(`Port: ${process.env.PORT}`)
-    console.log('Server is running on http://localhost:3333')
-})
+app.listen({ port: env.PORT }).then(() => {
+  console.log(`Port: ${process.env.PORT}`);
+  console.log('Server is running on http://localhost:3333');
+});
